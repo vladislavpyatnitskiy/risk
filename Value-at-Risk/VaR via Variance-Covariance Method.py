0b360365bafda8293c5310c1bb4c15f920e1ee94
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-09-12 11:33:57 +0500

    Enhance VaR_VC to support multiple tickers and date range
    Updated VaR_VC function to handle multiple tickers and optional date parameters for data retrieval.

--- Value-at-Risk/VaR via Variance-Covariance Method.py
+++ Value-at-Risk/VaR via Variance-Covariance Method.py
@@ -1,17 +1,34 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
+import yfinance as yf
 
-def VaR_VC(data, VaR=95, log_returns=True):
-    """
-    Calculate Value at Risk (VaR) via Variance-Covariance Method.
+def VaR_VC(y, s=None, e=None, VaR=95, log_returns=True):
     
-    :param data: A DataFrame or NumPy array of stock price data.
-    :param VaR: The desired confidence level for VaR (e.g., 95 for 95% VaR).
-    :param log_returns: Whether to calculate log returns (True by default).
-    :return: A DataFrame with VaR values.
-    """
+    p = pd.DataFrame()  # Create an empty DataFrame
+
+    # Loop for data extraction & Set up statements for start and end dates
+    for ticker in y:
+        if s is None and e is None:
+            # When neither start date nor end date is defined
+            data = yf.download(ticker, start="2007-01-01")
+        elif e is None:
+            data = yf.download(ticker, start=s) # Only start date is defined
+        elif s is None:
+            data = yf.download(ticker, end=e)  # When only end date is defined
+        else:
+            # When both start date and end date are defined
+            data = yf.download(ticker, start=s, end=e)
+
+        # Extract the Adjusted Close prices and add to the DataFrame
+        if not data.empty:
+            p[ticker] = data[('Close', f'{ticker}')]
+
+    p = p.dropna() # Drop rows with NA values
     
+    p.columns = y
+    
+    data = p
     # Check if there are less than 100 observations
     if len(data) < 100:
         print("Insufficient number of observations.")
@@ -37,7 +54,4 @@ def VaR_VC(data, VaR=95, log_returns=True):
     
     return var_values
 
-# Test with stock data
-# Example: stock_data is assumed to be a DataFrame with columns of stock prices
-# stock_data = pd.read_csv('your_data.csv')
-VaR_VC(result, 95)
+VaR_VC(y=["UNM", "AIG", "OMF", "MET", "HIG"], s="2022-01-01")
