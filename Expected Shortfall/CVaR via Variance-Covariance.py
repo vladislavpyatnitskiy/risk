b4175e92b9a7a32001bc0df39003b180f89d462d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-09-16 11:32:32 +0500

    Refactor CVaR_VC for flexible date handling
    Updated CVaR_VC function to accept dynamic start and end dates for data extraction. Enhanced handling of stock data retrieval and ensured proper calculation of log returns.

--- Expected Shortfall/CVaR via Variance-Covariance.py
+++ Expected Shortfall/CVaR via Variance-Covariance.py
@@ -1,31 +1,53 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
+import yfinance as yf
 
-def CVaR_VC(x, es=95, log_returns=True):
+def CVaR_VC(y, s=None, e=None, es=95, log_returns=True):
+    
+    p = pd.DataFrame()  # Create an empty DataFrame
+
+    # Loop for data extraction & Set up statements for start and end dates
+    for ticker in y:
+        if s is None and e is None:
+            # When neither start date nor end date is defined
+            data = yf.download(ticker, start="2007-01-01")
+        elif e is None:
+            data = yf.download(ticker, start=s) # Only start date is defined
+        elif s is None:
+            data = yf.download(ticker, end=e)  # When only end date is defined
+        else:
+            # When both start date and end date are defined
+            data = yf.download(ticker, start=s, end=e)
+
+        # Extract the Adjusted Close prices and add to the DataFrame
+        if not data.empty:
+            p[ticker] = data[('Close', f'{ticker}')]
+
+    p = p.dropna() # Drop rows with NA values
+    
+    p.columns = y
+  
     # Check whether there are less than 100 observations
-    if len(x) < 100:
+    if len(p) < 100:
         print("Insufficient number of observations.")
         return
     
     # Calculate log returns if requested and remove NAs
     if log_returns:
-        x = np.log(x / x.shift(1)).dropna()
+        p = np.log(p / p.shift(1)).dropna()
     
-    v = x.apply([np.mean, np.std], axis=0) # Means & Standard Deviations
+    v = p.apply([np.mean, np.std], axis=0) # Means & Standard Deviations
     
     l = [] # Set up list to contain future values
     
     # Calculate VaR using standard normal probabilities
-    for col in x.columns:
+    for col in p.columns:
         MEAN = v.loc['mean', col]
         SD = v.loc['std', col]
         l.append(MEAN - (norm.pdf(norm.ppf(es * .01)) / (1 - es * .01)) * SD)
     
     # Return DataFrame with CVaR values
-    return pd.DataFrame(l, index=x.columns, columns=[f"ES VC {es}%"])
+    return pd.DataFrame(l, index=p.columns, columns=[f"ES VC {es}%"])
 
-# Example usage:
-# Assuming stock_data is a pandas DataFrame with price data
-# stock_data = pd.read_csv("your_data.csv")
-CVaR_VC(result, 95)
+CVaR_VC(y=["UNM", "AIG", "OMF", "MET", "HIG"], s="2022-01-01")
